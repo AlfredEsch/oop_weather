d9c7575679cf44f7d7dec62323b7fb2531c691ac
AlfredEsch <alfred.eschbaum@khk.ee>
2022-11-10 09:59:13 +0200

    Css and added more stuff

--- classes/UI.js
+++ classes/UI.js
@@ -3,6 +3,9 @@ class UI {
         this.description = document.querySelector("#description")
         this.temp = document.querySelector("#temp")
         this.city = document.querySelector("#location")
+        this.windSpeed = document.querySelector("#windspeed")
+        this.winddeg = document.querySelector('#winddeg')
+        this.humidity = document.querySelector("#humidity")
     }
 
         drawWeather(data){
@@ -11,5 +14,8 @@ class UI {
             this.description.innerHTML = description;
             this.temp.innerHTML = celcius + '&deg;';
             this.city.innerHTML = data.name;
+            this.windSpeed.innerHTML = 'WindSpeed ' + data.wind.speed + " m/s";
+            this.winddeg.innerHTML = 'WindDeg ' + data.wind.deg + '&deg;';
+            this.humidity.innerHTML = 'Humditiy ' + data.main.humidity + "%";
     }
 }
